119bfa0235f86c862a5106656ec1d61bf7704143
Tung Nguyen <kenkz447@gmail.com>
2017-12-26 15:24:31 +0700

    image

--- Omi.Client/src/scripts/shared/modules/FileAndMedia/components/Image.tsx
+++ Omi.Client/src/scripts/shared/modules/FileAndMedia/components/Image.tsx
@@ -10,20 +10,12 @@ interface ImgProps {
     classNames?: string
     displayThumb?: boolean
 }
-const onChange = function (isVisible) {
-    console.log('Element is now %s', isVisible ? 'visible' : 'hidden')
-}
 
 export class Image extends React.Component<ImgProps> {
-    // style={{ width: this.props.fileEntityInfo.width, height: this.props.fileEntityInfo.height }}
     render() {
         return (
-            <div className="mw-100">
-                <VisibilitySensor onChange={onchange}>
-                    {({ isVisible }) =>
-                        <Img.default loader={<Spin />} className={this.props.classNames} src={`${window.baseUrl}${this.props.displayThumb ? this.props.fileEntityInfo.srcThumb : this.props.fileEntityInfo.src}`} />
-                    }
-                </VisibilitySensor>
+            <div className="mw-100 text-center">
+                <Img.default loader={<Spin />} className={this.props.classNames} src={`${window.baseUrl}${this.props.displayThumb ? this.props.fileEntityInfo.srcThumb : this.props.fileEntityInfo.src}`} />
             </div>
         )
     }
